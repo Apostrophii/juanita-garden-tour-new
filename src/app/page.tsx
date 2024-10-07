411441e1e790d8e9e60e2a4f5b71d5af574a7316
luke.foobar <luke.foobar@gmail.com>
2024-10-07 06:25:12 +0000

    Mobile styling tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,33 @@ const ADLaM = ADLaM_Display({
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center p-8 pb-20 sm:p-20">
-      <h1 className={`${ADLaM.className} text-4xl md:text-6xl font-bold text-center md:text-left mb-20`}>
-        juanita garden tour • spring &apos;25
+    <div className="flex flex-col items-center justify-center p-8 sm:p-20">
+      <h1 className={`${ADLaM.className} text-4xl md:text-6xl font-bold text-center md:text-left mb-8`}>
+        juanita garden tour
       </h1>
       <main className="w-full flex-1 flex md:flex-row-reverse flex-col md:gap-0 gap-12 items-center justify-center sm:items-start">
         <Image
-          className="rounded-md"
+          className="w-[140vw] max-w-[unset] rounded-md"
           src="/img/background-image.png"
           alt="Garden"
           width={800}
           height={600}
         />
         <WelcomeCard />
+        <Image
+          className="rounded-md"
+          src="/img/SeattleMap.png"
+          alt="Seattle Map"
+          width={800}
+          height={600}
+        />
+        <Image
+          className="rounded-md"
+          src="/img/JuanitaMap.png"
+          alt="Juanita Map"
+          width={800}
+          height={600}
+        />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
--- src/components/WelcomCard.tsx
+++ src/components/WelcomCard.tsx
@@ -2,16 +2,20 @@ export default function WelcomeCard() {
   return (
     <div className="bg-blue-200 bg-opacity-75 w-full flex flex-col items-start rounded-md py-7 px-5 flex-1 grow md:-mr-8 md:-mt-8 z-10 max-w-96 min-w-64">
       <h2 className="text-lg">
-        You&apos;re Invited!
+        You&apos;re Invited!<br/><br/>
       </h2>
+      <p>
+        We are highlighting North Kirkland's beautiful gardens for the first time in over 10 years.<br/><br/>
+        Money raised will go toward scholarships for Juanita High School students so come out and show them your support!<br/>
+      </p>
       <ul className="leading-7 mt-2">
         <li><b>Date:</b> June 7th</li>
-        <li><b>Time:</b> 10-3pm</li>
-        <li><b>Location:</b> Juanita Village Plaza</li>
-        <li><b>Price</b> <i>(suggested donation):</i>
+        <li><b>Time:</b> 10am - 3pm</li>
+        <li><b>Location:</b> <a href="https://maps.app.goo.gl/5JZjCNW6XtBTiZEN8" className="underline text-sky-600 decoration-sky-600">Juanita Village Plaza</a></li>
+        <li><b>Suggested donation:</b>
           <ul className="ml-3 leading-5 list-disc list-inside">
-            <li><b>In advance:</b> $15/Adult</li>
-            <li><b>Day of:</b> $20/Adult</li>
+            <li><b>In advance:</b> $15/adult</li>
+            <li><b>Day of:</b> $20/adult</li>
           </ul>
         </li>
       </ul>
