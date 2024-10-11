386b4830c955c677d4256796bca706508a0f213d
luke.foobar <luke.foobar@gmail.com>
2024-10-11 18:45:31 +0000

    Update button colors

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -7,7 +7,7 @@ export default function RegisterButton() {
     <Button
       variant="default"
       size="lg"
-      className="w-20 bg-amber-600 hover:bg-amber-800 text-white mb-4"
+      className="w-20 bg-[#f68623] hover:bg-[#d16609] text-white mb-4"
       onClick={() => alert('You can register soon...')}
     >
       Register
